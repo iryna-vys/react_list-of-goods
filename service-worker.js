1e8187a5e95e7e147bd5c5b0388ab5f288f9c910
iryna-vys <vys.iryna@gmail.com>
2021-05-25 23:25:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.a5d18b6cd7483ff45b1b873f10e16603.js"
 );
 
 self.addEventListener('message', (event) => {
